d89061afaf7908d8665d8b699c9db87a102baf52
Matute56a-jpg <peruca56a@gmail.com>
2025-09-01 19:34:35 -0300

    feat: integrar blog y actualizar configuración

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-fecha_creacion"]
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["titulo", "subtitulo", "autor", "contenido", "imagen"]
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["titulo", "subtitulo", "autor", "contenido", "imagen"]
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
